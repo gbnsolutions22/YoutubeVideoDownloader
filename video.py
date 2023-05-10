18f0c778a63a9c49f454d28a2bfb6e78d3e1767d
bhargav33 <bhargav@revidd.com>
2023-05-10 16:48:05 +0530

    initial commit with basic functionality of flask

---
+++ video.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request, send_file
+from pytube import YouTube
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/download', methods=['POST'])
+def download():
+    video_url = request.form['video_url']
+    yt = YouTube(video_url)
+    stream = yt.streams.get_highest_resolution()
+    filename = f'{yt.title}.mp4'
+    stream.download('./downloads/', filename)
+    return send_file('./downloads/' + filename, as_attachment=True)
+    
+if __name__ == "__main__":
+    app.run()
